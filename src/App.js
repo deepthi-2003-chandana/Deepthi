8056948e843f6b559048f06e1158ec397b308408
deepthi-2003-chandana <chandanaeruvaram@gmail.com>
2025-08-24 16:02:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,3 +6,19 @@ function App() {
 }
 
 export default App;
+
+function GigConnect() {
+  const [page, setPage] = useState("home");
+
+  return (
+    <div>
+      <Header setPage={setPage} />
+      {page === "home" && <Home setPage={setPage} />}
+      {page === "login" && <Login setPage={setPage} />}
+      {/* Add other pages here like Gigs, Profile, etc. */}
+      <Footer />
+    </div>
+  );
+}
+
+export default GigConnect;
